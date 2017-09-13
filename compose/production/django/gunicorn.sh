2cf24f8770648292f71e73afacf3d886775c22a6
Rafael Torres <rdtr.sis@gmail.com>
2017-09-12 22:07:01 -0300

    weak: Create a container enviroment to deploy on production
    fix: Added a entrypoint script with python to wait the database
    deployment
    
    fix: Create the nginx.conf file to deploy on production with custom requirements

--- compose/production/django/gunicorn.sh
+++ compose/production/django/gunicorn.sh
@@ -6,5 +6,4 @@ set -o nounset
 set -o xtrace
 
 python entrypoint.py
-python manage.py migrate
-python manage.py runserver 0.0.0.0:8000
+/usr/local/bin/gunicorn conf.wsgi -w 4 -b 0.0.0.0:5000 --chdir=/app
---
+++ compose/production/django/test/entrypoint.sh
@@ -0,0 +1,26 @@
+#!/bin sh
+
+set -o errexit
+set -o pipefail
+
+cmd="$@"
+
+function database_ready(){
+    python << END
+    import sys
+    import _mysql
+    try:
+        conn = _mysql.connect(db="$DB_DATA", user="$DB_USER", passwd="$DB_PASSWORD", host="db")
+    except _mysql_exceptions.OperationalError:
+        sys.exit(-1)
+    sys.exit(0)
+    END
+}
+
+until database_ready; do
+  >&2 echo "MySql is unavailable - sleeping"
+  sleep 1
+done
+
+>&2 echo "MySql is up - continuing..."
+exec $cmd
